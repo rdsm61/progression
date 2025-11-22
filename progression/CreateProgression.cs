589b2fd699ff7f1c2f8b322208ca1ff90161be68
rdsm61 <rdsameoff@gmail.com>
2025-11-23 01:29:44 +0400

    Добавьте файлы проекта.

---
+++ progression/CreateProgression.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace progression
+{   
+    class ArithmeticProgression
+    {
+        private double a0;
+        private double b;
+
+        public double Nmember(uint n)
+        {
+            return a0 + b * (n - 1);
+        }
+
+        public double Nsum(uint n)
+        {
+            return (a0 + Nmember(n)) * n / 2;
+        }
+
+        public void SetInitial(double init)
+        {
+            a0 = init;
+        }
+
+        public void SetDiff(double diff)
+        {
+            b = diff;
+        }
+
+        public double GetInitial()
+        {
+            return a0;
+        }
+
+        public double GetDiff()
+        {
+            return b;
+        }
+    }
+    class CreateProgression
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticProgression prog = CreateArithmeticProgression();
+            Console.WriteLine("Enter number of searching term ");
+            uint n = uint.Parse(Console.ReadLine());
+            Console.WriteLine("n term of the progression is {0}", prog.Nmember(n));
+            Console.WriteLine("Sum of n terms of the progression is {0}", prog.Nsum(n));
+        }
+
+        private static ArithmeticProgression CreateArithmeticProgression()
+        {
+            ArithmeticProgression prog = new ArithmeticProgression();
+            Console.WriteLine("Enter initial term of progression: ");
+            prog.SetInitial(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Enter difference of progression: ");
+            prog.SetDiff(double.Parse(Console.ReadLine()));
+
+            return prog;
+        }
+    }
+}
